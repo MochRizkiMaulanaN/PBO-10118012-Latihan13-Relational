1e51fbabbf90e6c129874ab32fd1fb51fd0b42f0
MochRizkiMaulanaN <55683135+MochRizkiMaulanaN@users.noreply.github.com>
2019-09-26 15:14:36 +0700

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package relational;
+
+/**
+ * Nama     : Moch Rizki Maulana N
+ * Kelas    : IF-1 2018
+ * Nim      : 10118012
+ * Deskripsi Program : Relational
+ *
+ * @author LENOVO
+ */
+public class Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 =10;
+        int bil2 =20;
+        System.out.println("a == b = "+(bil1 == bil2));
+        System.out.println("a != b = "+(bil1 != bil2));
+        System.out.println("a > b = "+(bil1 > bil2));
+        System.out.println("a < b = "+(bil1 < bil2));
+        System.out.println("b >= a = "+(bil2 >= bil1));
+        System.out.println("b <= a = "+(bil2 <= bil1));
+    }
+    
+}
